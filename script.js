e0e377f5f273cdac5bcce06c2b373bfb171cdb9e
Sujal gupta <sujalgupta5656@gmail.com>
2022-06-12 15:56:56 +0530

    just a small webpage

---
+++ script.js
@@ -0,0 +1,20 @@
+const drop = document.querySelector("#drop");
+const dropDown = document.querySelector(".drop-down");
+
+
+// drop.addEventListener("mouseover",()=>{
+//     dropDown.classList.add("show")
+//     console.log("drop");
+// })
+// drop.addEventListener("mouseleave",()=>{
+//     setTimeout(()=>{
+//         dropDown.classList.remove("show")
+//     },1000)
+//     console.log("drop");
+// })
+// dropDown.addEventListener("mouseover",()=>{
+//     dropDown.classList.add("show")
+//     console.log("drop");
+// })
+
+// console.log(drop,dropDown);
